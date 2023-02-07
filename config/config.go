9457f912010ade8a2b55e9795446e836ea312905
kai algodev <kai.algodev@gmail.com>
2023-02-07 17:58:41 +0330

    feat: adding config pkg

--- config/config.go
+++ config/config.go
@@ -1,4 +1,31 @@
 package config
 
+import "os"
+
 type Config struct {
+	PG
+	Credentials
+}
+
+type PG struct {
+	URL string
+}
+
+type Credentials struct {
+	SecretKey string
+}
+
+func New() *Config {
+	pgUrl := os.Getenv("pg_url")
+	secretKey := os.Getenv("secret_key")
+
+	config := &Config{
+		PG: PG{
+			URL: pgUrl,
+		},
+		Credentials: Credentials{
+			SecretKey: secretKey,
+		},
+	}
+	return config
 }
